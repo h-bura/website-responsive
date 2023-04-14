d81e355800439905358c53bf00d60df300d7b21e
Huseyın Bura <hsyn.bura@gmail.com>
2023-04-15 01:43:21 +0300

    add Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import "./contact.scss";
 
 function Contact() {
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email"></input>
+          <textarea type="text" placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message ? <span>Thans, I'll reply ASAP </span> : ""}
+        </form>
+      </div>
     </div>
   );
 }
